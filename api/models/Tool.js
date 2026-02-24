799855e72aefdd580ecb11488e75d7d414433612
om641228-ux <om641228@gmail.com>
2026-02-24 12:57:20 +0200

    Force fix api directory structure

--- api/models/Tool.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ToolSchema = new mongoose.Schema({
-  toolName: { type: String, required: true },
-  confidence: { type: String },
-  image: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Tool', ToolSchema);
